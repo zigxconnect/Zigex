e6c836eb696b26e82534fa06835545fc5172dc11
ABDUL FADIGA <tardzenyuyofficial@gmail.com>
2025-07-18 11:48:35 +0100

    dashboard layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "../_components/layout/Navbar";
 import { Footer } from "../_components/layout/Footer";
-// import { Navbar } from "./_components/layout/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
