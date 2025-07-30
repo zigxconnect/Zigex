74b3c8e46794dd23b2966bc2a46b94c78fa6928d
WillVerdze <verdzeleinyuyyefon@gmail.com>
2025-07-30 06:06:11 +0100

    Made the route dynamic and edited the database tables involved in the fetch

--- app/api/internships/[id]/route.ts
+++ app/api/internships/[id]/route.ts
@@ -2,28 +2,31 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/lib/supabase/client";
 const internships = 'internship'
 
-export default async function handler(req: NextApiRequest, res:NextApiResponse) {
-    const {query, method} = req;
-    const {id} = query;
+export  async function GET(req: NextApiRequest, res:NextApiResponse) {
+    const {method, query} = req;
+
+    const params = {id: query.id as string} 
+    const id = params;
 
     if (method === 'GET'){
         try{
-            if (typeof id !== 'string'){
+            if (!id || typeof id !== 'string'){
                 return res.status(400).json({message: 'Invalid'})
             }
 
             const {data: internship, error} = await supabase
             .from(internships)
-            .select('*')
+            .select(`*,company_profiles:company_id(*)`)
             .eq('id', id)
             .single();
+
 
             if (error) {
                 console.error('Database error', error.message);
                 if (error.code === 'PGRST116'){
                     return res.status(500).json({message: 'Internship not found'});
                 }
-                return res.status(500).json({message: 'Error getting Intenship: ${error.message}'});
+                return res.status(500).json({message: 'Error getting Intenship'});
             }
             if (!internship){
                 return res.status(400).json({message: "Internsgip not found"});
@@ -36,7 +39,7 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse) 
         }
     } else {
         res.setHeader('Allow',['GET']);
-        res.status(405).end('Method ${method} Not allowed');
+        res.status(405).end(' Not allowed');
     }
     
 }
