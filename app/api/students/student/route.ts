cf120af8bee091fa243b26dbdba7adfca4d496ef
WillVerdze <verdzeleinyuyyefon@gmail.com>
2025-08-02 16:04:03 +0100

    implemented the API endpoint to get all users

--- app/api/students/student/route.ts
+++ app/api/students/student/route.ts
@@ -1,1 +1,18 @@
 //  To GET a list of all other students
+// File: app/api/students/student/[id]/route.ts
+import { supabase } from '@/lib/supabase/client'
+
+// This is a function that provides a list of all students fetching data from the superbase student_profiles table
+export async function GET(
+  request: Request){
+    const {data: students, error} = await supabase
+      .from('student_profiles')
+      .select('*')
+      .order('created_at', { ascending: true })
+    if (error) {
+      console.error('Error fetching students:', error.message)
+      return Response.json({error: "Error fetching students"})
+    }
+
+    return Response.json(students, { status: 200 });
+}
