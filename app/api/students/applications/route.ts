38930d4898dc95bbe947e079c6c6839e24ef1722
WillVerdze <verdzeleinyuyyefon@gmail.com>
2025-08-05 03:41:34 +0100

    Just completed the API endpoint to allow for viewing all applications from a particular student.

--- app/api/students/applications/route.ts
+++ app/api/students/applications/route.ts
@@ -1,1 +1,27 @@
-// get all applications
+import { supabase } from '@/lib/supabase/client';
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const student_id = searchParams.get('student_id');
+
+    let query = supabase
+        .from('applications')
+        .select(`
+            *,
+            internship:internship_id(*)
+        `)
+        .order('created_at', { ascending: true });
+
+    if (student_id) {
+        query = query.eq('student_id', student_id);
+    }
+
+    const { data: applications, error } = await query;
+
+    if (error) {
+        console.error('Error fetching applications:', error.message);
+        return Response.json({ error: 'Error fetching applications' }, { status: 500 });
+    }
+
+    return Response.json(applications, { status: 200 });
+}
