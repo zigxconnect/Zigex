402bb4e9dc63e8f9941077559fb69493d94d2e71
iws-seed <iwstechnical7@gmail.com>
2025-08-20 13:05:49 +0100

    added and fixed the _component issue

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-
+// change
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-lg px-6 py-2.5 text-base font-semibold transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-60 shadow-sm hover:scale-105 cursor-pointer",
   {
