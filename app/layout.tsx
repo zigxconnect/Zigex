76c526ceb378e296c9e8c2508874944e8f48d791
iws-seed <iwstechnical7@gmail.com>
2025-08-20 10:38:52 +0100

    rectified merging issue with the feature/fupro-ai

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// import { Inter } from 'next/font/google'
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "futureProspect",
