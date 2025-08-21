c496ec716cdc3a535001ae5ee75319f6a4ba6f02
ABDUL FADIGA <tardzenyuyofficial@gmail.com>
2025-08-21 22:15:40 +0100

    feat:adjusted the liters erros

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,19 +1,18 @@
 import { redirect } from "next/navigation";
-// import FeaturedInternships from "../_components/sections/landing/FeaturesSection";
-// import BamendaHeroSection from "../_components/sections/landing/HeroSection";
-// import { WhyChoose } from "../_components/sections/landing/Whychoose";
-import { checkAuthStatus } from "@/lib/actions/auth.action";
-import { getProfileInfo, hasCompletedProfile } from "@/lib/actions/profile.actions";
+import {
+  getProfileInfo,
+  hasCompletedProfile,
+} from "@/lib/actions/profile.actions";
 import BamendaHeroSection from "@/components/sections/landing/HeroSection";
 import FeaturedInternships from "@/components/sections/landing/FeaturesSection";
 import { WhyChoose } from "@/components/sections/landing/Whychoose";
 
 export default async function LandingPage() {
-     const hasCompletedYourProfile=await hasCompletedProfile()
-     const profileInfo=await getProfileInfo()
-     console.log(`here is the data: ${profileInfo}`)
-     console.log(`your completed profile is: ${hasCompletedYourProfile}`)
-     if(hasCompletedYourProfile) return redirect("/dashboard")
+  const hasCompletedYourProfile = await hasCompletedProfile();
+  const profileInfo = await getProfileInfo();
+  console.log(`here is the data: ${profileInfo}`);
+  console.log(`your completed profile is: ${hasCompletedYourProfile}`);
+  if (hasCompletedYourProfile) return redirect("/dashboard");
   return (
     <div className="flex flex-col">
       <BamendaHeroSection />
