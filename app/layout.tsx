140533ace95e9207db178202e43a8016a582fd75
johnbrindi <johnbrindimazwewoh@gmail.com>
2025-12-20 13:01:37 -0800

    another fixes��

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 // import { Toaster } from "@/components/ui/sonner";
 import { Toaster } from "react-hot-toast";
 import { InstallPwaPopup } from "@/components/pwa/InstallPwaPopup";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
 
