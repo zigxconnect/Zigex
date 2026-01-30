a2763c4372eb17218da3288449332c529240e494
iws-seed <iwstechnical7@gmail.com>
2026-01-30 14:01:56 +0100

    fix: Add missing cn import in PostingCard

--- components/sections/admin/postings/PostingCard.tsx
+++ components/sections/admin/postings/PostingCard.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { toast } from "sonner";
 import {
   Briefcase,
