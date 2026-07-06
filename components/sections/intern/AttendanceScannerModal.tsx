b1a59f380fc84b769d1fa94e80779bb01930cb1a
iws-seed <iwstechnical7@gmail.com>
2026-07-06 03:05:59 +0100

    Add responsive padding around QR scanner modal on mobile

--- components/sections/intern/AttendanceScannerModal.tsx
+++ components/sections/intern/AttendanceScannerModal.tsx
@@ -14,7 +14,7 @@ export function AttendanceScannerModal({ children }: AttendanceScannerModalProps
       <DialogTrigger asChild>
         {children}
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md p-0 overflow-hidden bg-transparent border-0 shadow-none">
+      <DialogContent className="w-[calc(100%-2rem)] sm:w-full sm:max-w-md p-0 overflow-hidden bg-transparent border-0 shadow-none rounded-3xl">
         <AttendanceQRScanner />
       </DialogContent>
     </Dialog>
